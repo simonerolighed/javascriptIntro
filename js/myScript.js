4251be844554f4d63758863c45453dabc207af7a
Unknown <srolighedc@gmail.com>
2018-02-07 13:39:18 +0100

    redigering af js

--- js/myScript.js
+++ js/myScript.js
@@ -4,11 +4,31 @@
 
 // variabler
 var tal = 1;
+
+//booleans
 var erDetRigtigt = false;
-var tekst = "Her er min tekst";
+var erDetRigtigt = true;
+
+//en streng (noget tekst)
+var veritas = "Julemanden findes, og jorden er flad.";
+//Er kendetegnet med gåseøjene "..", Må ikke starte med et tal, kald variablerne med store bogstaver for hvert nyt ort 
+
+var simone = "<p> Simone er mega sej. </p>"; 
+
+// klister (+ bruges til at få variabler til at hænge sammen)
+var and = "<h1>Mit navn er Anders. Anders And. </h1>";
+
+// datatyper 
+var x = 0; 
 
 // kommentarer
+
 // regning 
+
+var ind = 2345;
+var ud = 9999; 
+// Kan bruges til webshops, matematiske udregninger. 
+
 // datoer
 // klistre strenge sammen
 // lister (scratch: data)
@@ -16,8 +36,10 @@ var tekst = "Her er min tekst";
 // hvis ... så ... ellers (scratch: operators)
 // loop (gentag / repeat i scratch)
 // objekter
-
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( "Tallet er lige nu: " + tal );
+console.log( veritas + " " + and);
+
+// skriv i browservinduet 
+document.write(and + simone  ); 
 
 // næste gang: events.
